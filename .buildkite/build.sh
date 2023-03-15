2f65ff9a59ce71738b1bcd00927b21069bee49e4
Jamil Appa <jamil.appa@zenotech.com>
2023-03-15 20:55:09 +0000

    download cmake

--- .buildkite/build.sh
+++ .buildkite/build.sh
@@ -7,6 +7,15 @@ git fetch --tags
 
 mkdir build
 pushd build
+CMAKE_VERSION=3.25.2
+DOWNLOAD_DIR=${PWD}
+if [ ! -f ${PWD}/cmake-${CMAKE_VERSION}-linux-x86_64/bin/cmake ]; then
+  if [ ! -f ${DOWNLOAD_DIR}/cmake-${CMAKE_VERSION}-linux-x86_64.sh ]; then
+    wget --no-check-certificate https://github.com/Kitware/CMake/releases/download/v${CMAKE_VERSION}/cmake-${CMAKE_VERSION}-linux-x86_64.sh -O ${DOWNLOAD_DIR}/cmake-${CMAKE_VERSION}-linux-x86_64.sh
+  fi
+  sh ${DOWNLOAD_DIR}/cmake-${CMAKE_VERSION}-linux-x86_64.sh --prefix=${PWD} --include-subdir
+fi
+export PATH=${PWD}/cmake-${CMAKE_VERSION}-linux-x86_64/bin:${PATH}
 cmake -DENABLE_openmpi=OFF -DENABLE_cuda=ON -DENABLE_ucx=OFF -DENABLE_efa=ON -DENABLE_psm2=OFF -DENABLE_nccl=ON -DENABLE_awsofinccl=ON ..
 cmake --build . -- VERBOSE=true
 ctest -R
